7f93c4548a9fcaa551f5e7075397225fc513c8de
Leandro Toledo <leptoledo@homtail.com>
2021-07-09 10:26:07 +0100

    roteamento e navegacao

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
@@ -23,10 +24,11 @@ export function NewRoom() {
               type="text"
               placeholder="Nome da sala"
             />
-            <Button type="submit">Nome da sala</Button>
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+            Quer entrar em uma sala existente? 
+            <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
